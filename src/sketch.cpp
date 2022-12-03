d766d8e83de96bdb6547fdf06b32779367bffc2f
Josie Thompson <dot.slash.josie@gmail.com>
2022-12-03 14:22:06 -0800

    added prototype expect function

--- src/sketch.cpp
+++ src/sketch.cpp
@@ -5,13 +5,101 @@
 
 // type constraints
 #include <ranges>
-#include <concepts>
 
 // data types and structures
 #include <unordered_set>
+#include <unordered_map>
 #include <string>
 
 namespace ranges = std::ranges;
+namespace views = std::views;
+
+namespace konbu {
+namespace just {
+/** Horizontal justification setting */
+enum class horizontal {
+    left,   /** Widget should be left-justified */
+    right,  /** Widget should be right-justified */
+    center, /** Widget should be centered horizontally */
+    fill    /** Widget should horizontally fill its layout */
+};
+/** Vertical justification setting */
+enum class vertical {
+    top,    /** Widget should be anchored to the top */
+    bottom, /** Widget should be anchored to the bottom */
+    center, /** Widget should be centered vertically */
+    fill    /** Widget should vertically fill its layout */
+};
+}
+struct layout {
+    just::horizontal horz = just::horizontal::left;
+    just::vertical vert = just::vertical::top;
+};
+
+template<typename container>
+concept can_push_back =
+requires(container & c, container::value_type const & v)
+{
+    c.push_back(v);
+};
+
+template<ranges::range error_output>
+requires ranges::output_range<error_output, YAML::Exception> and
+         can_push_back<error_output>
+
+void expect(YAML::Node const &config,
+            just::horizontal & horz,
+            error_output &errors)
+{
+    using justmap = std::unordered_map<std::string, just::horizontal>;
+    static justmap const justification {
+        { "left",   just::horizontal::left },
+        { "right",  just::horizontal::right },
+        { "center", just::horizontal::center },
+        { "fill",   just::horizontal::fill }
+    };
+    if (not config.IsScalar()) {
+        YAML::Exception const error{ config.Mark(), "Expecting a string" };
+        ranges::copy(views::single(error),
+                     std::back_inserter(errors));
+        return;
+    }
+    auto const search = justification.find(config.as<std::string>());
+    if (search != justification.end()) {
+        horz = search->second;
+        return;
+    }
+    std::stringstream message;
+    message << "expecting value to be one of the following: [";
+    std::string sep = "";
+    for (const auto & name : justification | views::keys) {
+        message << sep << name;
+        sep = ", ";
+    }
+    message << "]";
+    YAML::Exception const error{ config.Mark(), message.str() };
+    ranges::copy(views::single(error),
+                 std::back_inserter(errors));
+}
+
+void print_error(std::string const & message) {
+    std::cout << message << "\n";
+}
+}
+
+namespace just = konbu::just;
+std::ostream & operator<<(std::ostream & os, just::horizontal const & horz)
+{
+    using namemap = std::unordered_map<just::horizontal, std::string>;
+    static namemap const names{
+        { just::horizontal::left,   "left" },
+        { just::horizontal::right,  "right" },
+        { just::horizontal::center, "center" },
+        { just::horizontal::fill,   "fill" }
+    };
+    os << names.find(horz)->second;
+    return os;
+}
 
 int main()
 {
@@ -33,17 +121,14 @@ int main()
         std::cout << "Expecting \"horizontal\" settings to be a string\n";
         return EXIT_FAILURE;
     }
-    std::string const horizontal_value = horizontal_config.as<std::string>();
-    if (not setting_names.contains(horizontal_value)) {
-        std::cout << "Expecting \"horizontal\" value to be one of: [";
-        std::string sep = "";
-        for (const auto& name : setting_names) {
-            std::cout << sep << name;
-            sep = ", ";
-        }
-        std::cout << "]\n";
-        return EXIT_FAILURE;
-    }
-    std::cout << "Using \"" << horizontal_value << "\" for horizontal value\n";
+    namespace just = konbu::just;
+
+    std::vector<YAML::Exception> errors;
+    just::horizontal horz;
+    konbu::expect(horizontal_config, horz, errors);
+    ranges::for_each(errors | views::transform(&YAML::Exception::what),
+                     konbu::print_error);
+
+    std::cout << "Using \"" << horz << "\" for horizontal value\n";
     return EXIT_SUCCESS;
 }
